f70992700006e695d4f13e66eb6841baec350cec
Marcelo Schambeck <mschambeck@gmail.com>
2022-02-04 01:50:51 -0300

    Cache

---
+++ src/main/java/com/schambeck/dna/web/conf/CacheConfiguration.java
@@ -0,0 +1,9 @@
+package com.schambeck.dna.web.conf;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+class CacheConfiguration {
+}
