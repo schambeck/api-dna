ee0b2a923705165b735d5552f695cd5611928197
Marcelo Schambeck <marcelo.demoraesschambeck@thomsonreuters.com>
2022-08-05 22:26:20 -0300

    WebSecurityConfigurerAdapter deprecation

--- src/main/java/com/schambeck/dna/web/conf/SecurityConfig.java
+++ src/main/java/com/schambeck/dna/web/conf/SecurityConfig.java
@@ -1,20 +1,21 @@
 package com.schambeck.dna.web.conf;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 
 @Profile("!test")
 @EnableWebSecurity
-class SecurityConfig extends WebSecurityConfigurerAdapter {
+class SecurityConfig {
 
     @Value("${auth0.audience}")
     private String audience;
@@ -22,14 +23,15 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuer;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .mvcMatchers(GET, "/mutant/stats").hasAuthority("SCOPE_read:stats")
                 .mvcMatchers(POST, "/mutant").hasAuthority("SCOPE_create:mutant")
                 .mvcMatchers(GET, "/mutant").hasAuthority("SCOPE_list:mutant")
                 .anyRequest().permitAll()
                 .and().oauth2ResourceServer().jwt().decoder(jwtDecoder());
+        return http.build();
     }
 
     private JwtDecoder jwtDecoder() {
