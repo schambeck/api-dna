b63737059c6c374378c2b2f9d75d2f44de4033e8
Marcelo Schambeck <mschambeck@gmail.com>
2022-08-24 06:59:08 -0300

    CrossOrigin

--- src/main/java/com/schambeck/dna/web/controller/DnaController.java
+++ src/main/java/com/schambeck/dna/web/controller/DnaController.java
@@ -24,7 +24,7 @@ import static org.springframework.http.HttpStatus.*;
 @Validated
 @RestController
 @RequestMapping("/mutant")
-@CrossOrigin(origins = {"http://localhost:4200", "https://schambeck.github.io/ui-dna"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://schambeck.github.io"})
 class DnaController {
 
     private final DnaService service;
